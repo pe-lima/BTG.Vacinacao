c1e02635eb704ec854821565a245068fa696d75c
Pedro Lima <pedro.limma.dev@gmail.com>
2025-04-09 17:55:17 -0300

    feat(core): Refactor repository interfaces to improve structure and consistency

---
+++ src/BTG.Vacinacao.Core/Interfaces/Repositories/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTG.Vacinacao.Core.Interfaces.Repositories
+{
+    public interface IUnitOfWork
+    {
+        IPersonRepository Person { get; }
+        IVaccineRepository Vaccine { get; }
+       
+        Task<int> CommitAsync();
+
+    }
+}
