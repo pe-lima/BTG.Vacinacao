c152b35d2ebc7cb947d6664ed53fc70abd144df4
Pedro Lima <pedro.limma.dev@gmail.com>
2025-04-09 16:36:45 -0300

    feat(entities): update Person class to include Cpf property and constructor overload; add Vaccine class and IVaccineRepository interface

---
+++ src/BTG.Vacinacao.Core/Interfaces/Repository/IVaccineRepository.cs
@@ -0,0 +1,15 @@
+﻿using BTG.Vacinacao.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTG.Vacinacao.Core.Interfaces.Repository
+{
+    public interface IVaccineRepository
+    {
+        Task AddAsync(Vaccine vaccine);
+        Task<Vaccine> GetByIdAsync(Guid id);
+    }
+}
