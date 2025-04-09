0a903ebf6935b47b98ad12afeb6ea0a9890bb8fe
Pedro Lima <pedro.limma.dev@gmail.com>
2025-04-09 17:56:15 -0300

    test(application): Refactor RegisterPerson and RegisterVaccine command handler tests to use IUnitOfWork for repository interactions

--- Tests/BTG.Vacinacao.UnitTests/Application/Handlers/VaccineHandler/RegisterVaccineCommandHandlerTests.cs
+++ Tests/BTG.Vacinacao.UnitTests/Application/Handlers/VaccineHandler/RegisterVaccineCommandHandlerTests.cs
@@ -1,13 +1,11 @@
 ﻿using BTG.Vacinacao.Application.Commands.VaccineCommand;
 using BTG.Vacinacao.Application.Handlers.VaccineHandler;
 using BTG.Vacinacao.Core.Entities;
-using BTG.Vacinacao.Core.Interfaces.Repository;
+using BTG.Vacinacao.Core.Interfaces.Repositories;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace BTG.Vacinacao.UnitTests.Application.Handlers.VaccineHandler
 {
@@ -19,16 +17,22 @@ namespace BTG.Vacinacao.UnitTests.Application.Handlers.VaccineHandler
             // Arrange
             var command = new RegisterVaccineCommand("COVID-19 Vaccine", "121234");
 
-            var mockRepo = new Mock<IVaccineRepository>();
-            mockRepo.Setup(r => r.AddAsync(It.IsAny<Vaccine>()))
-                    .Returns(Task.CompletedTask);
-            
-            var handler = new RegisterVaccineCommandHandler(mockRepo.Object);
+            var mockVaccineRepo = new Mock<IVaccineRepository>();
+            mockVaccineRepo.Setup(r => r.ExistsByCodeAsync(command.Code)).ReturnsAsync(false);
+            mockVaccineRepo.Setup(r => r.AddAsync(It.IsAny<Vaccine>())).Returns(Task.CompletedTask);
+
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork.Setup(u => u.Vaccine).Returns(mockVaccineRepo.Object);
+            mockUnitOfWork.Setup(u => u.CommitAsync()).ReturnsAsync(1);
+
+            var handler = new RegisterVaccineCommandHandler(mockUnitOfWork.Object);
+
             // Act
             var result = await handler.Handle(command, CancellationToken.None);
 
             // Assert
-            mockRepo.Verify(r => r.AddAsync(It.IsAny<Vaccine>()), Times.AtLeastOnce());
+            mockVaccineRepo.Verify(r => r.AddAsync(It.IsAny<Vaccine>()), Times.Once);
+            mockUnitOfWork.Verify(u => u.CommitAsync(), Times.Once);
             Assert.NotNull(result);
             Assert.Equal("COVID-19 Vaccine", result.Name);
             Assert.Equal("121234", result.Code);
