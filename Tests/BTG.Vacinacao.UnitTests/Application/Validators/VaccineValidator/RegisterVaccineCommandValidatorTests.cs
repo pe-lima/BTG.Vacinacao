df15ac1c0f15b4b8c8596df4c5a75dfd2d1398d3
Pedro Lima <pedro.limma.dev@gmail.com>
2025-04-09 20:22:34 -0300

    feat(tests): Add unit tests for RegisterVaccination and RegisterVaccine command handlers and validators

--- Tests/BTG.Vacinacao.UnitTests/Application/Validators/VaccineValidator/RegisterVaccineCommandValidatorTests.cs
+++ Tests/BTG.Vacinacao.UnitTests/Application/Validators/VaccineValidator/RegisterVaccineCommandValidatorTests.cs
@@ -1,6 +1,7 @@
 ﻿using BTG.Vacinacao.Application.Commands.VaccineCommand;
 using BTG.Vacinacao.Application.Validators.VaccineValidator;
 using FluentValidation.TestHelper;
+using Xunit;
 
 namespace BTG.Vacinacao.UnitTests.Application.Validators.VaccineValidator
 {
@@ -17,25 +18,20 @@ namespace BTG.Vacinacao.UnitTests.Application.Validators.VaccineValidator
         public void Should_Pass_When_Name_And_Code_Are_Valid()
         {
             var command = new RegisterVaccineCommand("Covid-19", "123456");
+
             var result = _validator.TestValidate(command);
 
-            result.ShouldNotHaveValidationErrorFor(x => x.Name);
-            result.ShouldNotHaveValidationErrorFor(x => x.Code);
+            result.ShouldNotHaveAnyValidationErrors();
         }
 
-        [Fact]
-        public void Should_Fail_When_Name_Is_Empty()
+        [Theory]
+        [InlineData("")]
+        [InlineData("co")]
+        [InlineData(null)]
+        public void Should_Fail_When_Name_Is_Invalid(string invalidName)
         {
-            var command = new RegisterVaccineCommand("", "123456");
-            var result = _validator.TestValidate(command);
+            var command = new RegisterVaccineCommand(invalidName, "123456");
 
-            result.ShouldHaveValidationErrorFor(x => x.Name);
-        }
-
-        [Fact]
-        public void Should_Fail_When_Name_Is_Too_Short()
-        {
-            var command = new RegisterVaccineCommand("co", "123456");
             var result = _validator.TestValidate(command);
 
             result.ShouldHaveValidationErrorFor(x => x.Name);
@@ -45,54 +41,35 @@ namespace BTG.Vacinacao.UnitTests.Application.Validators.VaccineValidator
         public void Should_Fail_When_Name_Is_Too_Long()
         {
             var command = new RegisterVaccineCommand(new string('C', 101), "123456");
+
             var result = _validator.TestValidate(command);
 
             result.ShouldHaveValidationErrorFor(x => x.Name);
         }
 
-        [Fact]
-        public void Should_Fail_When_Code_Is_Empty()
+        [Theory]
+        [InlineData("")]
+        [InlineData("123")]
+        [InlineData("1234567")]
+        [InlineData("ABC123")]
+        [InlineData("12-456")]
+        public void Should_Fail_When_Code_Is_Invalid(string invalidCode)
         {
-            var command = new RegisterVaccineCommand("Covid-19", "");
+            var command = new RegisterVaccineCommand("Covid-19", invalidCode);
+
             var result = _validator.TestValidate(command);
 
             result.ShouldHaveValidationErrorFor(x => x.Code);
         }
 
         [Fact]
-        public void Should_Fail_When_Code_Is_Short()
+        public void Should_Pass_When_Code_Is_Valid()
         {
-            var command = new RegisterVaccineCommand("Covid-19", "123");
+            var command = new RegisterVaccineCommand("Covid-19", "123456");
+
             var result = _validator.TestValidate(command);
 
-            result.ShouldHaveValidationErrorFor(x => x.Code);
-        }
-
-        [Fact]
-        public void Should_Fail_When_Code_Is_Too_Long()
-        {
-            var command = new RegisterVaccineCommand("Covid-19", "1234567");
-            var result = _validator.TestValidate(command);
-
-            result.ShouldHaveValidationErrorFor(x => x.Code);
-        }
-
-        [Fact]
-        public void Should_Fail_When_Code_Has_Letters()
-        {
-            var command = new RegisterVaccineCommand("Covid-19", "ABC123");
-            var result = _validator.TestValidate(command);
-
-            result.ShouldHaveValidationErrorFor(x => x.Code);
-        }
-
-        [Fact]
-        public void Should_Fail_When_Code_Has_SpecialCharacters()
-        {
-            var command = new RegisterVaccineCommand("Covid-19", "12-456");
-            var result = _validator.TestValidate(command);
-
-            result.ShouldHaveValidationErrorFor(x => x.Code);
+            result.ShouldNotHaveValidationErrorFor(x => x.Code);
         }
     }
 }
