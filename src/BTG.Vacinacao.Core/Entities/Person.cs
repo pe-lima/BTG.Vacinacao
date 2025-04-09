cfed92c3a43ee8557ec5e88e3d5cd8f01766f4a0
Pedro Lima <pedro.limma.dev@gmail.com>
2025-04-09 11:53:22 -0300

    feat(core): create Person entity and repository interface

---
+++ src/BTG.Vacinacao.Core/Entities/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTG.Vacinacao.Core.Entities
+{
+    public class Person : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public Person(string name)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            CreatedAt = DateTime.UtcNow;
+        }
+    }
+}
