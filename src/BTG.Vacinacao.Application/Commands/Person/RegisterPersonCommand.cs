f59af49147138bf4e07b5dbd1b2b2922a7149a57
Pedro Lima <pedro.limma.dev@gmail.com>
2025-04-09 12:03:58 -0300

    feat(application): implement RegisterPersonCommand and handler

---
+++ src/BTG.Vacinacao.Application/Commands/Person/RegisterPersonCommand.cs
@@ -0,0 +1,13 @@
+﻿using BTG.Vacinacao.Application.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTG.Vacinacao.Application.Commands.Person
+{
+    public record RegisterPersonCommand(string Name) : IRequest<PersonDto>;
+    
+}
