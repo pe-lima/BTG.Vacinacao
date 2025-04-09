bffa29126ce633b130e64f0c28d4758f3fe6fa18
Pedro Lima <pedro.limma.dev@gmail.com>
2025-04-09 20:21:46 -0300

    feat(application): Add RegisterVaccination command, handler, DTO, and validator for vaccination registration

---
+++ src/BTG.Vacinacao.Application/DTOs/VaccinationDto.cs
@@ -0,0 +1,13 @@
+﻿using BTG.Vacinacao.Core.Enums;
+
+namespace BTG.Vacinacao.Application.DTOs
+{
+    public class VaccinationDto
+    {
+        public Guid Id { get; set; }
+        public string PersonName { get; set; }
+        public string VaccineName { get; set; }
+        public DoseType DoseType { get; set; }
+        public DateTime ApplicationDate { get; set; }
+    }
+}
