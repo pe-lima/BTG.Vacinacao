f642487033d8470dd25ed16370569054859f0568
Pedro Lima <pedro.limma.dev@gmail.com>
2025-04-09 14:07:57 -0300

    chore: estrutura base com EF, SQLite, IoC e DI

---
+++ src/BTG.Vacinacao.Infra/Context/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using BTG.Vacinacao.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTG.Vacinacao.Infra.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)  
+            : base(options) { }
+
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+            
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Cascade;
+            }
+            
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ src/BTG.Vacinacao.Infra/Mappings/PersonMapping.cs
@@ -0,0 +1,32 @@
+﻿using BTG.Vacinacao.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTG.Vacinacao.Infra.Mappings
+{
+    public class PersonMapping : IEntityTypeConfiguration<Person>
+    {
+        public void Configure(EntityTypeBuilder<Person> builder)
+        {
+            builder.ToTable("Persons");
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(p => p.CreatedAt)
+                .IsRequired();
+
+            builder.Property(p => p.UpdatedAt)
+                .IsRequired(false);
+
+        }
+    }
+}
