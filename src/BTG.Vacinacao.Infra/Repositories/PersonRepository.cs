1599ec4f5fc0179d647e6fcf18ec44deffb310ee
Pedro Lima <pedro.limma.dev@gmail.com>
2025-04-09 14:15:37 -0300

    feat(application): set up initial project structure with FluentValidation, MediatR, and Entity Framework Core; implement Person entity and repository

---
+++ src/BTG.Vacinacao.Infra/Repositories/PersonRepository.cs
@@ -0,0 +1,23 @@
+﻿using BTG.Vacinacao.Core.Entities;
+using BTG.Vacinacao.Core.Interfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTG.Vacinacao.Infra.Repositories
+{
+    public class PersonRepository : IPersonRepository
+    {
+        public Task AddAsync(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Person> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
