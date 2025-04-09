d99a338950232984090143ecb20dee054115a0ce
Pedro Lima <pedro.limma.dev@gmail.com>
2025-04-09 20:22:24 -0300

    feat(presentation): Add VaccinationController with RegisterVaccination endpoint

---
+++ src/BTG.Vacinacao.Presentation/Controllers/VaccinationController.cs
@@ -0,0 +1,25 @@
+﻿using BTG.Vacinacao.Application.Commands.VaccinationCommand;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BTG.Vacinacao.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VaccinationController: ControllerBase
+    {
+        public IMediator _mediator;
+        public VaccinationController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterVaccination([FromBody] RegisterVaccinationCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return CreatedAtAction(nameof(RegisterVaccination), new { id = result.Id }, result);
+        }
+    }
+}
