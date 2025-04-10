5a87713cb37b8f4c8b05c955ff6aa18c6095ec90
Pedro Lima <pedro.limma.dev@gmail.com>
2025-04-10 12:40:20 -0300

    feat(person): Implement query handlers and validators for retrieving persons by CPF and all persons

---
+++ src/BTG.Vacinacao.Application/Validators/PersonValidator/GetPersonByCpfQueryValidator.cs
@@ -0,0 +1,21 @@
+﻿using BTG.Vacinacao.Application.Queries.PersonQuery;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTG.Vacinacao.Application.Validators.PersonValidator
+{
+    public class GetPersonByCpfQueryValidator : AbstractValidator<GetPersonByCpfQuery>
+    {
+        public GetPersonByCpfQueryValidator()
+        {
+            RuleFor(x => x.Cpf)
+                .NotEmpty().WithMessage("CPF is required.")
+                .Length(11).WithMessage("Cpf must be exactly 11 characters.")
+                .Matches(@"^\d{11}$").WithMessage("Cpf must contain only numeric characters.");
+        }
+    }
+}
