5f319cf13eb3612ffaaf6d307d9a2904e5f90c6d
Pedro Lima <pedro.limma.dev@gmail.com>
2025-04-10 14:26:14 -0300

    refactor(repository): Simplify method implementations in VaccinationRepository

--- src/BTG.Vacinacao.Infra/Repositories/VaccinationRepository.cs
+++ src/BTG.Vacinacao.Infra/Repositories/VaccinationRepository.cs
@@ -10,21 +10,25 @@ namespace BTG.Vacinacao.Infra.Repositories
     {
         public VaccinationRepository(ApplicationDbContext context) : base(context) { }
 
-        public async Task<bool> ExistsAsync(Guid personId, Guid vaccineId, DoseType doseType)
-        {
-            return await _dbSet.AnyAsync(v =>
+        public async Task<bool> ExistsAsync(Guid personId, Guid vaccineId, DoseType doseType) =>
+            await _dbSet.AnyAsync(v =>
                 v.PersonId == personId &&
                 v.VaccineId == vaccineId &&
                 v.DoseType == doseType
             );
-        }
 
-        public async Task<List<Vaccination>> GetByPersonIdWithVaccineAsync(Guid personId)
-        {
-            return await _dbSet
+        public async Task<List<Vaccination>> GetByPersonIdWithVaccineAsync(Guid personId )=> 
+            await _dbSet
                 .Include(v => v.Vaccine)
                 .Where(v => v.PersonId == personId)
                 .ToListAsync();
-        }
+
+        public async Task<List<Vaccination>> GetByPersonIdAsync(Guid personId) =>
+            await _dbSet
+                .Where(v => v.PersonId == personId)
+                .ToListAsync();
+
+        public void RemoveRange(IEnumerable<Vaccination> vaccinations) => 
+            _dbSet.RemoveRange(vaccinations);
     }
 }
