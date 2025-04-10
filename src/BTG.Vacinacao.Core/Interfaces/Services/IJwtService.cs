5c1d2fa6a2d331ca58833717a453ccbaed4b17bc
Pedro Lima <pedro.limma.dev@gmail.com>
2025-04-10 17:28:54 -0300

    feat(service): Implement JwtService and IJwtService for token generation

---
+++ src/BTG.Vacinacao.Core/Interfaces/Services/IJwtService.cs
@@ -0,0 +1,14 @@
+﻿using BTG.Vacinacao.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTG.Vacinacao.Core.Interfaces.Services
+{
+    public interface IJwtService
+    {
+        (string Token, DateTime Expiration) GenerateToken(User user);
+    }
+}
---
+++ src/BTG.Vacinacao.CrossCuting/Configurations/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using BTG.Vacinacao.Core.Interfaces.Services;
+using BTG.Vacinacao.CrossCutting.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTG.Vacinacao.CrossCutting.Configurations
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddCrossCutting(this IServiceCollection services)
+        {
+            services.AddScoped<IJwtService, JwtService>();
+            return services;
+        }
+    }
+}
