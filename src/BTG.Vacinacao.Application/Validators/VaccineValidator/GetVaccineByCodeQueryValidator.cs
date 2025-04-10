4c86edc709c782ed7abca2349df0c74d53e053aa
Pedro Lima <pedro.limma.dev@gmail.com>
2025-04-10 12:39:27 -0300

    feat(application): Implement query handlers and validators for fetching vaccines by code and all vaccines

---
+++ src/BTG.Vacinacao.Application/Validators/VaccineValidator/GetVaccineByCodeQueryValidator.cs
@@ -0,0 +1,20 @@
+﻿using BTG.Vacinacao.Application.Queries.VaccineQuery;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTG.Vacinacao.Application.Validators.VaccineValidator
+{
+    public class GetVaccineByCodeQueryValidator : AbstractValidator<GetVaccineByCodeQuery>
+    {
+        public GetVaccineByCodeQueryValidator()
+        {
+            RuleFor(x => x.Code)
+                .NotEmpty().WithMessage("Code is required.")
+                .Matches(@"^\d{6}$").WithMessage("Code must be a 6-digit numeric value.");
+        }
+    }
+}
