0e1df8849a4b2a80b5ea100689207c0a248ba0ae
Pedro Lima <pedro.limma.dev@gmail.com>
2025-04-10 17:29:28 -0300

    feat(database): Add User entity, migration, and seeder for user management

---
+++ src/BTG.Vacinacao.Infra/Migrations/20250410194511_CreateUserTableAndSendUserBySeed.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BTG.Vacinacao.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateUserTableAndSendUserBySeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Username = table.Column<string>(type: "TEXT", nullable: false),
+                    PasswordHash = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- src/BTG.Vacinacao.Infra/Repositories/UserRepository.cs
+++ src/BTG.Vacinacao.Infra/Repositories/UserRepository.cs
@@ -1,5 +1,6 @@
 ﻿using BTG.Vacinacao.Core.Entities;
 using BTG.Vacinacao.Core.Interfaces.Repositories;
+using BTG.Vacinacao.Infra.Context;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -11,7 +12,7 @@ namespace BTG.Vacinacao.Infra.Repositories
 {
     public class UserRepository : Repository<User>, IUserRepository
     {
-        public UserRepository(DbContext context) : base(context) { }
+        public UserRepository(ApplicationDbContext context) : base(context) { }
 
         public async Task<User?> GetByUsernameAsync(string username) => 
             await _dbSet.FirstOrDefaultAsync(u => u.Username == username);
