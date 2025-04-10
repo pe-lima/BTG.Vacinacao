62eb4a5ab775ec47691ad46cd90742d8d543d4fb
Pedro Lima <pedro.limma.dev@gmail.com>
2025-04-10 14:54:41 -0300

    feat(vaccination): Add Id property to VaccinationRecordDto and map it in GetVaccinationCardByCpfQueryHandler

--- src/BTG.Vacinacao.Application/DTOs/VaccinationRecordDto.cs
+++ src/BTG.Vacinacao.Application/DTOs/VaccinationRecordDto.cs
@@ -4,6 +4,7 @@ namespace BTG.Vacinacao.Application.DTOs
 {
     public class VaccinationRecordDto
     {
+        public Guid Id { get; set; }
         public string VaccineName { get; set; }
         public string DoseType { get; set; }
         public DateTime ApplicationDate { get; set; }
