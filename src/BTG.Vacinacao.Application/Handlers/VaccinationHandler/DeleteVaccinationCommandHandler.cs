5b07a8415e32bc6150079b7ad764b14655f0265f
Pedro Lima <pedro.limma.dev@gmail.com>
2025-04-10 14:55:08 -0300

    feat(vaccination): Implement DeleteVaccination command, handler, and validator

---
+++ src/BTG.Vacinacao.Application/Handlers/VaccinationHandler/DeleteVaccinationCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using BTG.Vacinacao.Application.Commands.VaccinationCommand;
+using BTG.Vacinacao.Core.Interfaces.Repositories;
+using FluentValidation;
+using MediatR;
+
+namespace BTG.Vacinacao.Application.Handlers.VaccinationHandler
+{
+    public class DeleteVaccinationCommandHandler : IRequestHandler<DeleteVaccinationCommand, Unit>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteVaccinationCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Unit> Handle(DeleteVaccinationCommand request, CancellationToken cancellationToken)
+        {
+            var vaccination = await _unitOfWork.Vaccination.GetByIdAsync(request.VaccinationId);
+
+            if (vaccination is null)
+                throw new ValidationException("Vaccination record not found.");
+
+            _unitOfWork.Vaccination.Remove(vaccination);
+            await _unitOfWork.CommitAsync();
+
+            return Unit.Value;
+        }
+    }
+}
+
