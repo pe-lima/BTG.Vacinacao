17df3175dd9fa64ed52cde5b7845395d8b1c5b18
Pedro Lima <pedro.limma.dev@gmail.com>
2025-04-11 09:00:04 -0300

    feat(cross-cutting) : Create a CPF Validator to use in All CpfValidations

---
+++ src/BTG.Vacinacao.CrossCuting/Extensions/ValidationExtensions.cs
@@ -0,0 +1,24 @@
+﻿using BTG.Vacinacao.CrossCutting.Utils;
+using FluentValidation;
+
+namespace BTG.Vacinacao.CrossCutting.Extensions
+{
+    public static class ValidationExtensions
+    {
+        public static IRuleBuilderOptions<T, string> ApplyCpfRules<T>(this IRuleBuilder<T, string> ruleBuilder)
+        {
+            return ruleBuilder
+                .NotEmpty().WithMessage("Cpf is required.")
+                .Length(11).WithMessage("Cpf must be exactly 11 characters.")
+                .Matches(@"^\d{11}$").WithMessage("Cpf must contain only numeric characters.")
+                .Must(CpfUtils.IsValid).WithMessage("CPF is incorrect.");
+        }
+
+        public static IRuleBuilderOptions<T, string> ApplyVaccineCodeRules<T>(this IRuleBuilder<T, string> ruleBuilder)
+        {
+            return ruleBuilder
+                .NotEmpty().WithMessage("Code is required.")
+                .Matches(@"^\d{6}$").WithMessage("Code must be a 6-digit numeric value.");
+        }
+    }
+}
